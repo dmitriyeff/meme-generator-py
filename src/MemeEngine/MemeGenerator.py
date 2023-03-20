0c419180d57991048f52f6652c3b90a8e1597a70
Denis <deniss.dmitrijevs@pipedrive.com>
2023-03-20 10:35:21 +0200

    fix pip pycodestyle MemeGenerator

--- src/MemeEngine/MemeGenerator.py
+++ src/MemeEngine/MemeGenerator.py
@@ -1,11 +1,18 @@
 from PIL import Image, ImageDraw, ImageFont
 import random
 
+
 class MemeGenerator:
     def __init__(self, output_dir):
-        self.out_path = output_dir      
-    
-    def make_meme(self, img_path: str, body: str, author: str, width=500) -> str:
+        self.out_path = output_dir
+
+    def make_meme(
+        self,
+        img_path: str,
+        body: str,
+        author: str,
+        width=500
+    ) -> str:
         """Create a Postcard With a Text Greeting
 
         Arguments:
@@ -16,26 +23,30 @@ class MemeGenerator:
         Returns:
             str -- the file path to the output image.
         """
-        
         try:
             with Image.open(img_path) as img:
                 im = img.getdata()
                 width, height = im.size
                 pixels = width * height
-                
+
                 if (pixels > 500):
                     """ Resize an image """
                     size = width, height
                     img = img.resize(size)
-                
-                   
+
                 draw = ImageDraw.Draw(img)
                 font = ImageFont.load_default()
-                draw.text((random.randint(0, width), random.randint(0, height)), f"{body} - {author}", font=font, fill='white')
+                draw.text(
+                    (random.randint(0, width),
+                     random.randint(0, height)),
+                    f"{body} - {author}",
+                    font=font,
+                    fill='white'
+                )
 
                 image_name = random.getrandbits(64)
                 img.save(f"{self.out_path}/{image_name}.jpg")
-                
+
                 return f"{self.out_path}/{image_name}.jpg"
         except Exception as error:
             raise Exception(error)
