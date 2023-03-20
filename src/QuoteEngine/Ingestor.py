642564593881506cfd5fd3ca73df76e1903b51d5
Denis <deniss.dmitrijevs@pipedrive.com>
2023-03-20 10:41:21 +0200

    fix pip pycodestyle Ingestor

--- src/QuoteEngine/Ingestor.py
+++ src/QuoteEngine/Ingestor.py
@@ -1,27 +1,36 @@
 from .IngestorInterface import IngestorInterface
-from QuoteEngine.Ingestors import TxtIngestor, CsvIngestor, PdfIngestor, DocxIngestor
+from QuoteEngine.Ingestors import (
+    TxtIngestor,
+    CsvIngestor,
+    PdfIngestor,
+    DocxIngestor
+)
 
-class Ingestor(IngestorInterface):    
+
+class Ingestor(IngestorInterface):
     ingestors = {
         'txt': TxtIngestor,
         'csv': CsvIngestor,
         'pdf': PdfIngestor,
         'docx': DocxIngestor,
     }
-    
-    
+
     @classmethod
     def parse(cls, path):
         """ Select appropriate ingestor class to parse provided file """
         file_extension = cls.get_file_extension(path)
-        
+
         try:
             ingestor = cls.ingestors.get(file_extension)
-            
+
             if not ingestor:
-                """ Raise an exception if file extension parser is not provided """
-                raise Exception(f"File extension with name: `{file_extension}`, is not supported")
-            
+                """Raise an exception
+                if file extension parser is not provided
+                """
+                raise Exception(
+                    f"File extension `{file_extension}`, is not supported"
+                )
+
             return cls.ingestors.get(file_extension).parse(path)
         except Exception as error:
             print(error)
