f7a5c05305ac085016f2dddadcb365c8535237f9
Denis <deniss.dmitrijevs@pipedrive.com>
2023-03-20 10:47:19 +0200

    fix pip pycodestyle QuoteModel

--- src/QuoteEngine/QuoteModel.py
+++ src/QuoteEngine/QuoteModel.py
@@ -1,8 +1,8 @@
 class QuoteModel:
-	""" Encapsulate body and author """
-	def __init__(self, body, author):
-		self.body = body
-		self.author = author
-	
-	def __str__(self):
-		return f"{self.body} - {self.author}"
+    """ Encapsulate body and author """
+    def __init__(self, body, author):
+        self.body = body
+        self.author = author
+
+    def __str__(self):
+        return f"{self.body} - {self.author}"
